0523ea420396920c88f24bdf173062ee0b0466e0
= <phattrinh9@gmail.com>
2021-01-05 10:37:07 +0700

    add pug view

--- 1.HelloWorld/index.js
+++ 1.HelloWorld/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.set('view engine', 'pug')
+//app.set('views', './views')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
